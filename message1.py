f7c6f5285eb51d1429872f6bc4fac62a122082f7
Eric Wu <ericwugo@gmail.com>
2022-02-17 12:12:19 +0800

    用twilio 傳簡訊

---
+++ message1.py
@@ -0,0 +1,17 @@
+#用 twilio 傳 簡訊
+#1 . 檔名不能用 與 twilio 相同檔名
+#2. 不要用 import os 的程式
+#3. 到 twilio 網站 查詢 sid 與 token
+from twilio.rest import Client
+account_sid = 'A'
+auth_token = 'f'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="EricWu Go Go Go !.",
+                     from_='+18596952721',
+                     to='+8'
+                 )
+
+print(message.sid)
